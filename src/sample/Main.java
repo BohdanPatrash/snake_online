d2f8087b54ec9bc9bd434031c65c5a459f39d66e
Богдан <youshi.kukarachi@gmail.com>
2018-06-21 13:01:09 +0300

    begins

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,9 +18,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args){
-        System.out.println("go");
-        System.out.println("kolja ti pochemy ne sdal exameni");
-        System.out.println("kanfliktik");
+
         launch(args);
     }
 }
