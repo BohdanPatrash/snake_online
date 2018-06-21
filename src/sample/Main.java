30ae0026aa03bc4ad7dddd6bbf35d9564a72f1e6
Богдан <youshi.kukarachi@gmail.com>
2018-06-21 13:21:45 +0300

    lalalalalalalalalalala

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args){
-
+        System.out.println("sas");
         System.out.println("sosi ska");
         launch(args);
     }
