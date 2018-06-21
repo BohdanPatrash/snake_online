b65c3cc2836a57ac37d0f09fc6abc4fc0665dd01
Mykola <mykola.medynsky@gmail.com>
2018-06-21 13:39:59 +0200

    +menu

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,13 +12,12 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Super snake");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 300, 350));
         primaryStage.show();
     }
 
 
     public static void main(String[] args){
-
         launch(args);
     }
 }
