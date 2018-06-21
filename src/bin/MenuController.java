87707c75fddd06de4ad6f665f0e87f70ba43e3ec
Mykola <mykola.medynsky@gmail.com>
2018-06-21 18:51:28 +0200

    button Exit

--- src/bin/MenuController.java
+++ src/bin/MenuController.java
@@ -1,6 +1,26 @@
 package bin;
 
-public class MenuController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+public class MenuController {
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button ExitButton;
+
+    @FXML
+    private Button InstructionsButton;
+
+    @FXML
+    private Button StartButton;
+
+    public void  ExitBattonPress(){
+        Stage stage = (Stage) ExitButton.getScene().getWindow();
+        stage.close();
+    }
 
 }
